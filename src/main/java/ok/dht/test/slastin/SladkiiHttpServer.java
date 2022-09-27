35b0e06dc47691d2be01f58d167a016a880065e4
Aleksandr Slastin <a.slastin@bk.ru>
2022-09-27 11:14:24 +0300

    test passes with lsm

--- src/main/java/ok/dht/test/slastin/SladkiiHttpServer.java
+++ src/main/java/ok/dht/test/slastin/SladkiiHttpServer.java
@@ -1,6 +1,9 @@
 package ok.dht.test.slastin;
 
-import ok.dht.test.slastin.lsm.exception.DaoException;
+import jdk.incubator.foreign.MemorySegment;
+import ok.dht.test.slastin.lsm.BaseEntry;
+import ok.dht.test.slastin.lsm.Config;
+import ok.dht.test.slastin.lsm.MemorySegmentDao;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;
@@ -9,24 +12,27 @@ import one.nio.http.Path;
 import one.nio.http.Request;
 import one.nio.http.Response;
 import one.nio.net.Session;
+import one.nio.util.Utf8;
 
 import java.io.IOException;
 
 public class SladkiiHttpServer extends HttpServer {
+    private static final Response BAD_REQUEST = new Response(Response.BAD_REQUEST, Response.EMPTY);
+    private static final Response NOT_FOUND = new Response(Response.NOT_FOUND, Response.EMPTY);
+    private static final Response CREATED = new Response(Response.CREATED, Response.EMPTY);
+    private static final Response ACCEPTED = new Response(Response.ACCEPTED, Response.EMPTY);
 
-    private final SladkiiComponent component;
+    private final MemorySegmentDao dao;
 
-    public SladkiiHttpServer(
-            final HttpServerConfig httpServerConfig,
-            final SladkiiComponent component
-    ) throws IOException {
+    public SladkiiHttpServer(final HttpServerConfig httpServerConfig, final Config daoConfig) throws IOException {
         super(httpServerConfig);
-        this.component = component;
+        dao = new MemorySegmentDao(daoConfig);
     }
 
     @Override
     public void handleDefault(Request request, HttpSession session) throws IOException {
-        session.sendResponse(badRequest());
+        Response response = new Response(Response.BAD_REQUEST, Response.EMPTY);
+        session.sendResponse(response);
     }
 
     @Override
@@ -44,37 +50,40 @@ public class SladkiiHttpServer extends HttpServer {
             final Request request
     ) {
         if (id.isBlank()) {
-            return badRequest();
+            return BAD_REQUEST;
         }
-        try {
-            return switch (request.getMethod()) {
-                case Request.METHOD_GET -> component.get(id);
-                case Request.METHOD_PUT -> component.put(id, request);
-                case Request.METHOD_DELETE -> component.delete(id);
-                default -> badRequest();
-            };
-        } catch (DaoException e) {
-            return internalError();
+        return switch (request.getMethod()) {
+            case Request.METHOD_GET -> get(id);
+            case Request.METHOD_PUT -> put(id, request);
+            case Request.METHOD_DELETE -> delete(id);
+            default -> BAD_REQUEST;
+        };
+    }
+
+    public Response get(final String id) {
+        var entry = dao.get(Utils.toMemorySegment(id));
+        return entry != null ? new Response(Response.OK, entry.value().toByteArray()) : NOT_FOUND;
+    }
+
+    public Response put(final String id, final Request request) {
+        var entry = new BaseEntry<>(Utils.toMemorySegment(id), Utils.toMemorySegment(request.getBody()));
+        dao.upsert(entry);
+        return CREATED;
+    }
+
+    public Response delete(final String id) {
+        var entry = new BaseEntry<>(Utils.toMemorySegment(id), null);
+        dao.upsert(entry);
+        return ACCEPTED;
+    }
+
+    private static class Utils {
+        public static MemorySegment toMemorySegment(final String val) {
+            return MemorySegment.ofArray(Utf8.toBytes(val));
         }
-    }
 
-    static Response badRequest() {
-        return new Response(Response.BAD_REQUEST, Response.EMPTY);
-    }
-
-    static Response internalError() {
-        return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
-    }
-
-    static Response notFound() {
-        return new Response(Response.NOT_FOUND, Response.EMPTY);
-    }
-
-    static Response created() {
-        return new Response(Response.CREATED, Response.EMPTY);
-    }
-
-    static Response accepted() {
-        return new Response(Response.ACCEPTED, Response.EMPTY);
+        public static MemorySegment toMemorySegment(final byte[] bytes) {
+            return MemorySegment.ofArray(bytes);
+        }
     }
 }
