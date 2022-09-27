fecab0a15251eed86ca903e4e900dc76e1b555c3
Aleksandr Slastin <a.slastin@bk.ru>
2022-09-27 11:14:24 +0300

    test passes with lsm

--- src/main/java/ok/dht/test/slastin/SladkiiService.java
+++ src/main/java/ok/dht/test/slastin/SladkiiService.java
@@ -3,25 +3,39 @@ package ok.dht.test.slastin;
 import ok.dht.Service;
 import ok.dht.ServiceConfig;
 import ok.dht.test.ServiceFactory;
+import ok.dht.test.slastin.lsm.Config;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
 import one.nio.server.AcceptorConfig;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.concurrent.CompletableFuture;
 
 public class SladkiiService implements Service {
+    public static Path DEFAULT_DAO_DIRECTORY = Path.of("dao");
+    public static long DEFAULT_FLUSH_THRESHOLD_BYTES = 4 * 1024 * 1024;
 
-    private final ServiceConfig config;
+    private final ServiceConfig serviceConfig;
+    private final Config daoConfig;
+
     private HttpServer server;
 
-    public SladkiiService(ServiceConfig config) {
-        this.config = config;
+    public SladkiiService(final ServiceConfig serviceConfig) {
+        this(serviceConfig, new Config(
+                serviceConfig.workingDir().resolve(DEFAULT_DAO_DIRECTORY),
+                DEFAULT_FLUSH_THRESHOLD_BYTES)
+        );
+    }
+
+    public SladkiiService(final ServiceConfig serviceConfig, final Config daoConfig) {
+        this.serviceConfig = serviceConfig;
+        this.daoConfig = daoConfig;
     }
 
     @Override
     public CompletableFuture<?> start() throws IOException {
-        server = new SladkiiHttpServer(makeConfig(config.selfPort()));
+        server = new SladkiiHttpServer(makeHttpServerConfig(serviceConfig.selfPort()), daoConfig);
         server.start();
         return CompletableFuture.completedFuture(null);
     }
@@ -32,7 +46,7 @@ public class SladkiiService implements Service {
         return CompletableFuture.completedFuture(null);
     }
 
-    private static HttpServerConfig makeConfig(int port) {
+    private static HttpServerConfig makeHttpServerConfig(int port) {
         HttpServerConfig httpConfig = new HttpServerConfig();
         AcceptorConfig acceptor = new AcceptorConfig();
         acceptor.port = port;
