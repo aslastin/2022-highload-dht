35b748734517cc93101f6895721750576912d17d
Aleksandr Slastin <a.slastin@bk.ru>
2022-09-28 12:57:39 +0300

    improve service

--- src/main/java/ok/dht/test/slastin/SladkiiHttpServer.java
+++ src/main/java/ok/dht/test/slastin/SladkiiHttpServer.java
@@ -1,23 +1,12 @@
 package ok.dht.test.slastin;
 
 import ok.dht.test.slastin.lsm.DaoException;
-import one.nio.http.HttpServer;
-import one.nio.http.HttpServerConfig;
-import one.nio.http.HttpSession;
-import one.nio.http.Param;
-import one.nio.http.Path;
-import one.nio.http.Request;
-import one.nio.http.Response;
+import one.nio.http.*;
 import one.nio.net.Session;
 
 import java.io.IOException;
 
 public class SladkiiHttpServer extends HttpServer {
-    static final Response BAD_REQUEST = new Response(Response.BAD_REQUEST, Response.EMPTY);
-    static final Response NOT_FOUND = new Response(Response.NOT_FOUND, Response.EMPTY);
-    static final Response CREATED = new Response(Response.CREATED, Response.EMPTY);
-    static final Response ACCEPTED = new Response(Response.ACCEPTED, Response.EMPTY);
-    static final Response INTERNAL_ERROR = new Response(Response.INTERNAL_ERROR, Response.EMPTY);
 
     private final SladkiiComponent component;
 
@@ -31,7 +20,7 @@ public class SladkiiHttpServer extends HttpServer {
 
     @Override
     public void handleDefault(Request request, HttpSession session) throws IOException {
-        session.sendResponse(BAD_REQUEST);
+        session.sendResponse(BAD_REQUEST());
     }
 
     @Override
@@ -49,17 +38,37 @@ public class SladkiiHttpServer extends HttpServer {
             final Request request
     ) {
         if (id.isBlank()) {
-            return BAD_REQUEST;
+            return BAD_REQUEST();
         }
         try {
             return switch (request.getMethod()) {
                 case Request.METHOD_GET -> component.get(id);
                 case Request.METHOD_PUT -> component.put(id, request);
                 case Request.METHOD_DELETE -> component.delete(id);
-                default -> BAD_REQUEST;
+                default -> BAD_REQUEST();
             };
         } catch (DaoException e) {
-            return INTERNAL_ERROR;
+            return INTERNAL_ERROR();
         }
+    }
+
+    static Response BAD_REQUEST() {
+        return new Response(Response.BAD_REQUEST, Response.EMPTY);
+    }
+
+    static Response INTERNAL_ERROR() {
+        return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+    }
+
+    static Response NOT_FOUND() {
+        return new Response(Response.NOT_FOUND, Response.EMPTY);
+    }
+
+    static Response CREATED() {
+        return new Response(Response.CREATED, Response.EMPTY);
+    }
+
+    static Response ACCEPTED() {
+        return new Response(Response.ACCEPTED, Response.EMPTY);
     }
 }
