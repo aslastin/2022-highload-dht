f8e66ae451a6a40f30d8dfcc4d4278585137a03c
Aleksandr Slastin <a.slastin@bk.ru>
2022-10-05 17:45:27 +0300

    fixes

--- src/main/java/ok/dht/test/slastin/wrk2/putb.lua
+++
@@ -1,9 +0,0 @@
-wrk.method = "PUT"
-
-counter = 1000000
-request = function()
-    wrk.body = string.rep("MiyLittlePony", 1000) .. counter
-    path = "/v0/entity?id=" .. counter
-    counter = counter + 1
-    return wrk.format(nil, path)
-end
