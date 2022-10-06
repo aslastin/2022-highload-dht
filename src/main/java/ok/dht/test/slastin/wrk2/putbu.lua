fcee6c9e4c426dac59bb778326649cd4b62cac95
Aleksandr Slastin <a.slastin@bk.ru>
2022-10-06 05:40:18 +0300

    update report

--- src/main/java/ok/dht/test/slastin/wrk2/putbu.lua
+++ src/main/java/ok/dht/test/slastin/wrk2/putbu.lua
@@ -3,8 +3,8 @@ wrk.method = "PUT"
 math.randomseed(os.time())
 
 request = function()
-    id = math.random(50000000)
-    wrk.body = string.rep("MiyLittlePony", 2000) .. id
+    id = math.random(5000000000000)
+    wrk.body = string.rep("MiyLittlePony", 5000) .. id
     path = "/v0/entity?id=" .. id
     return wrk.format(nil, path)
 end
